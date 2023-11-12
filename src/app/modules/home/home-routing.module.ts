0d37068cdce2a8a731b85d8a01199a00d763acda
unknown <simonbustamante21@gmail.com>
2023-11-12 19:00:11 -0300

    Integracion de Router Outlet. page Home outlet -> page tracks

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
+//Parametros -> dashboard/:iduser 
+
 const routes: Routes = [
+  
   {
-    path:'dashboard', //Parametros -> dashboard/:iduser
-    component: HomePageComponent
-  }
+    // path:'dashboard', //TODO: localhost:4200/home/dashboard
+    // component: HomePageComponent
+    path: 'tracks',
+    loadChildren: () => import('@modules/tracks/tracks.module').then( m => m.TracksModule)
+  },
+  {
+    path: 'favorites',
+    loadChildren: () => import('@modules/favorites/favorites.module').then( m => m.FavoritesModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('@modules/history/history.module').then( m => m.HistoryModule)
+  },
 ];
 
 @NgModule({
